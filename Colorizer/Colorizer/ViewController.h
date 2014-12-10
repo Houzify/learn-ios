00593e9632cc26c834b9d3adca326eac2f61b5b5
Shailesh <kochhar@gmail.com>
2014-12-10 14:18:57 +0530

    Added simple color changer app to learn iOS basics.

---
+++ Colorizer/Colorizer/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Colorizer
+//
+//  Created by Shailesh on 24/11/14.
+//  Copyright (c) 2014 houzify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+- (IBAction)colorChanged:(id)sender;
+
+@end
+
